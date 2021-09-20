4bf3653b6d139c46e79dc8e65beaa59246b50bba
mikeolivotto <michaelolivotto@gmail.com>
2021-09-20 20:48:07 +1000

    Randomise the cocktail after given a ingredient

--- src/index.js
+++ src/index.js
@@ -47,24 +47,19 @@ function getCocktailName() {
     });
 }
 
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then((res) =>
-//   res.json()
-// );
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-// .then(res => res.json())
-
-// .then((data) => console.log(data.drinks[0].strDrink))
-
 
 // button selector
 
 let button = document.getElementById("submit")
-let cocktail = document.getElementById("cocktail")
-
+let container = document.querySelector(".container")
+let cocktail = document.createElement("div")
+cocktail.setAttribute("id", "cocktail")
+container.appendChild(cocktail)
 
 // event listener for button, get the user input
 button.addEventListener("click", (event) => {
     event.preventDefault();
+    
     let ingredients = document.getElementById("ingredients").value
     // console.log(ingredients)
     cocktail.innerHTML = ""
@@ -76,7 +71,14 @@ button.addEventListener("click", (event) => {
 function findCocktail(ingredient) {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient)
     .then(res => res.json())
-    .then((data) => getCocktail(data.drinks[0].idDrink))
+    // pull a random drink containing the ingredient entered
+    .then((data) => {
+        let length = data.drinks.length
+        let random = Math.floor(Math.random() * length)
+        console.log(random)
+        getCocktail(data.drinks[random].idDrink)
+    })
+    
     .catch(() => console.log("No cocktails include that ingredient"))
 
 }
