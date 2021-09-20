b95679cecaaff620c2d8e634d7b8dd713b8bfe46
mikeolivotto <michaelolivotto@gmail.com>
2021-09-20 15:12:08 +1000

    basic logic to get drink based on user input

--- index.js
+++ index.js
@@ -3,5 +3,24 @@
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 .then(res => res.json())
-.then((data) => console.log(data.drinks[0].strDrink))
+// .then((data) => console.log(data.drinks[0].strDrink))
 
+// button selector
+let button = document.getElementById("submit")
+
+// event listener for button, get the user input
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    let ingredients = document.getElementById("ingredients").value
+    // console.log(ingredients)
+    return getCocktail(ingredients)
+})
+
+
+// get cocktail based on single ingredient
+function getCocktail(ingredient) {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient)
+    .then(res => res.json())
+    .then((data) => console.log(data.drinks[0].strDrink))
+
+}
