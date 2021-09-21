50683105e9d402f4abffdfb3556036e7a654d8e6
saman-zdf <samanfyz68@gmail.com>
2021-09-21 08:13:25 +0800

    move the code from index to alphabet js, added one curly brace in css file to resolve the issue on css too

---
+++ src/alphabet.js
@@ -0,0 +1,62 @@
+// Sam's code
+// get all the element from html that we need to use as a DOM
+const card = document.querySelector('#card');
+const links = document.querySelectorAll('#link');
+const error = document.querySelector('#error');
+// fetch list of cocktail by the first letter
+function listOfDrink(letter) {
+  for (let i = 0; i <= 20; i++) {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + letter)
+      .then((response) => response.json())
+      .then((data) => getListOfDink(data, i))
+      .catch(
+        () => (error.innerText = 'Something went wrong, Please try again🙂')
+      );
+  }
+}
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    card.innerHTML = '';
+    switch (link.textContent) {
+      case 'A':
+        listOfDrink('a');
+        break;
+      case 'B':
+        listOfDrink('b');
+        break;
+      case 'C':
+        listOfDrink('c');
+        break;
+      case 'D':
+        listOfDrink('d');
+        break;
+      default:
+        break;
+    }
+  });
+});
+// get all the drink with the first letter
+function getListOfDink(data, i) {
+  let image = data.drinks[i].strDrinkThumb;
+  let name = data.drinks[i].strDrink;
+  let glass = data.drinks[i].strGlass;
+  let instructions = data.drinks[i].strInstructions;
+  card.innerHTML += `
+    <div class="col-sm">
+      <div class="card mt-4 card-custom" style="width: 18rem">
+      <img class="card-img-top" src="${image}" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title" >${name}</h5>
+          <p class="card-text">
+            ${glass}
+          </p>
+          </div>
+        <p class="text-custom">
+        <span> Instuction:</span>
+        </br>
+          ${instructions}
+        </p>
+    </div>
+  `;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
-fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-  .then((res) => res.json())
-  .then((data) => console.log(data.drinks[0].strDrink));
+// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+// .then(res => res.json())
+// .then((data) => console.log(data.drinks[0].strDrink))
 
 // button selector
 let button = document.getElementById('submit');
@@ -9,89 +8,17 @@ let cocktail = document.getElementById('cocktail');
 
 // event listener for button, get the user input
 button.addEventListener('click', (event) => {
-  cocktail.innerHTML = '';
   event.preventDefault();
   let ingredients = document.getElementById('ingredients').value;
   // console.log(ingredients)
-  cocktail.classList.add('cocktial-info');
   return getCocktail(ingredients);
-let nameInput = document.querySelector("#search-cocktail-name");
-let cocktailName = document.querySelector("#cocktail-name");
-let cocktailCategory = document.querySelector("#cocktail-category");
-let cocktailInstructions = document.querySelector("#cocktail-instructions");
-let cocktailAlcoholic = document.querySelector("#cocktail-alcoholic");
-let cocktailNameButton = document.querySelector(".search-form");
-let cocktailIngredient = document.querySelector("#cocktail-ingredient");
-let cocktailInfo = document.querySelector(".cocktail-info");
-let cocktailImage = document.createElement("img");
-
-cocktailNameButton.addEventListener("submit", (event) => {
-  cocktailImage.removeAttribute("src");
-  event.preventDefault();
-  getCocktailName();
-});
-
-function getCocktailName() {
-  const cocktailApi =
-    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-  // fecth data from api based cocktail
-  fetch(cocktailApi + nameInput.value)
-    .then((response) => response.json())
-    .then((data) => {
-      //   console.log(data.drinks[0]);
-      let drinks = data.drinks[0];
-      let imgSrc = drinks.strDrinkThumb;
-      let ingredients = [];
-      // push ingredients (and their measurements) to arrays
-      for (let i = 1; i <= 15; i++) {
-        if (drinks[`strIngredient${i}`]) {
-          ingredients.push(drinks[`strIngredient${i}`]);
-        }
-      }
-      // add date to html
-      if (nameInput.value) {
-        cocktailImage.setAttribute("src", imgSrc);
-        cocktailImage.setAttribute("style", "height: 400px; width: 400px");
-        cocktailInfo.appendChild(cocktailImage);
-        cocktailName.innerHTML = drinks.strDrink;
-        cocktailCategory.innerHTML = drinks.strCategory;
-        cocktailInstructions.innerHTML = drinks.strInstructions;
-        cocktailAlcoholic.innerHTML = `Alcohol content: ${drinks.strAlcoholic}`;
-        cocktailIngredient.innerHTML = `Ingredients: ${ingredients.join(", ")}`;
-      } else {
-        nameInput.value === null;
-        alert("Please enter a cocktail name");
-      }
-    });
-}
-
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then((res) =>
-//   res.json()
-// );
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-// .then(res => res.json())
-
-// .then((data) => console.log(data.drinks[0].strDrink))
-
-// button selector
-
-let button = document.getElementById("submit");
-let cocktail = document.getElementById("cocktail");
-
-// event listener for button, get the user input
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  let ingredients = document.getElementById("ingredients").value;
-  // console.log(ingredients)
-  cocktail.innerHTML = "";
-  return findCocktail(ingredients);
-
 });
 
 // get cocktail based on single ingredient
 function getCocktail(ingredient) {
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
+  fetch(
+    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient
+  )
     .then((res) => res.json())
     .then((data) => displayCocktail(data.drinks[0].idDrink))
     .catch(() => console.log('No cocktails include that ingredient'));
@@ -117,7 +44,6 @@ function getRecipe(data) {
   for (let i = 1; i <= 15; i++) {
     if (drink[`strIngredient${i}`]) {
       ingredients.push(drink[`strIngredient${i}`]);
-
     }
     if (drink[`strMeasure${i}`]) {
       measures.push(drink[`strMeasure${i}`]);
@@ -155,68 +81,6 @@ function getRecipe(data) {
   cocktail.appendChild(directions);
 }
 
-// Sam's code
-// get all the element from html that we need to use as a DOM
-const card = document.querySelector('#card');
-const links = document.querySelectorAll('#link');
-const error = document.querySelector('#error');
-// fetch list of cocktail by the first letter
-function listOfDrink(letter) {
-  for (let i = 0; i <= 20; i++) {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + letter)
-      .then((response) => response.json())
-      .then((data) => getListOfDink(data, i))
-      .catch(
-        () => (error.innerText = 'Something went wrong, Please try again🙂')
-      );
-  }
-}
-
-links.forEach((link) => {
-  link.addEventListener('click', () => {
-    card.innerHTML = '';
-    switch (link.textContent) {
-      case 'A':
-        listOfDrink('a');
-        break;
-      case 'B':
-        listOfDrink('b');
-        break;
-      case 'C':
-        listOfDrink('c');
-        break;
-      case 'D':
-        listOfDrink('d');
-        break;
-      default:
-        break;
-    }
-  });
-});
-// get all the drink with the first letter
-function getListOfDink(data, i) {
-  let image = data.drinks[i].strDrinkThumb;
-  let name = data.drinks[i].strDrink;
-  let glass = data.drinks[i].strGlass;
-  let instructions = data.drinks[i].strInstructions;
-  card.innerHTML += `
-    <div class="col-sm">
-      <div class="card mt-4 card-custom" style="width: 18rem">
-      <img class="card-img-top" src="${image}" alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title" >${name}</h5>
-          <p class="card-text">
-            ${glass}
-          </p>
-          </div>
-        <p class="text-custom">
-        <span> Instuction:</span>
-        </br>
-          ${instructions}
-        </p>
-    </div>
-  `;
-    }
     if (drink[`strMeasure${i}`]) {
       measures.push(drink[`strMeasure${i}`]);
     }
